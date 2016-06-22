2c3b74ca23d8db2aceb6934b18afa88cc4d36a57
AneesAhammed <aneesahammed@gmail.com>
2016-06-22 16:33:50 +0400

    The 'Onboarding experience'

---
+++ OnBoard/OnBoard/Fragments.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+
+namespace OnBoard
+{
+	public class FirstFragment : Android.Support.V4.App.Fragment
+	{
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your fragment here
+		}
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			// Use this to return your custom view for this Fragment
+			return inflater.Inflate(Resource.Layout.FirstFragment, container, false);
+
+			//return base.OnCreateView (inflater, container, savedInstanceState);
+		}
+	}
+
+	public class SecondFragment : Android.Support.V4.App.Fragment
+	{
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your fragment here
+		}
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			// Use this to return your custom view for this Fragment
+			return inflater.Inflate(Resource.Layout.SecondFragment, container, false);
+
+			//return base.OnCreateView (inflater, container, savedInstanceState);
+		}
+	}
+
+	public class ThirdFragment : Android.Support.V4.App.Fragment
+	{
+		public override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Create your fragment here
+		}
+
+		public override View OnCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+		{
+			// Use this to return your custom view for this Fragment
+			 return inflater.Inflate(Resource.Layout.ThirdFragment, container, false);
+
+			//return base.OnCreateView (inflater, container, savedInstanceState);
+		}
+	}
+}
+
---
+++ OnBoard/OnBoard/MainActivity.cs
@@ -0,0 +1,115 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V4.App;
+using Android.Support.V4.View;
+using System;
+using Android.Graphics;
+using Android.Views;
+
+namespace OnBoard
+{
+	[Activity (Theme = "@style/Theme.NoTitle",Label = "OnBoard", MainLauncher = true, Icon = "@mipmap/icon")]
+	public class MainActivity : Android.Support.V4.App.FragmentActivity,Android.Support.V4.View.ViewPager.IOnPageChangeListener
+	{
+		private static  int NUM_PAGES = 3;
+		private ImageView [] _carouselIndicator = new ImageView [NUM_PAGES];
+		private int _currentPosition;
+
+
+		protected override void OnCreate (Bundle savedInstanceState)
+		{
+			base.OnCreate (savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+
+			var fragments = new Android.Support.V4.App.Fragment []{
+				new FirstFragment(),
+				new SecondFragment(),
+				new ThirdFragment(),
+			};
+
+			var viewPager = FindViewById<Android.Support.V4.View.ViewPager> (Resource.Id.myPager);
+			viewPager.Adapter = new SlideAdapter (base.SupportFragmentManager, fragments);
+			viewPager.AddOnPageChangeListener (this);
+			int selectedPos = viewPager.CurrentItem;
+
+
+			LinearLayout carouselLayout = (LinearLayout)FindViewById<LinearLayout>(Resource.Id.carousel_indicator_layout);
+
+			for (int i = 0; i < carouselLayout.ChildCount; i++) {
+				var imgView = (ImageView)carouselLayout.GetChildAt (i);
+				if(selectedPos==i){
+					imgView.Selected = true;
+					_currentPosition = i;
+				}
+				_carouselIndicator [i] = imgView;
+			}
+
+		}
+
+		void ViewPager.IOnPageChangeListener.OnPageScrolled (int position, float positionOffset, int positionOffsetPixels)
+		{
+			
+		}
+
+		void ViewPager.IOnPageChangeListener.OnPageScrollStateChanged (int state)
+		{
+			
+		}
+
+		void ViewPager.IOnPageChangeListener.OnPageSelected (int position)
+		{
+			UpdateCarouselIndicator (position);
+		}
+
+		void UpdateCarouselIndicator (int position)
+		{
+			_carouselIndicator [_currentPosition].Selected = false;
+			_currentPosition = position;
+			_carouselIndicator [position].Selected = true;
+		}
+}
+
+
+
+
+
+
+
+
+
+
+	//adapter class
+	class SlideAdapter : FragmentStatePagerAdapter
+	{
+		Android.Support.V4.App.Fragment [] _fragments;
+
+
+		public SlideAdapter (Android.Support.V4.App.FragmentManager fm,
+							Android.Support.V4.App.Fragment [] fragments) : base (fm)
+		{
+			this._fragments = fragments;
+		}
+
+
+		public override int Count {
+			get {
+				return _fragments.Length;
+			}
+		}
+
+		public override Android.Support.V4.App.Fragment GetItem (int position)
+		{
+			return _fragments [position];
+		}
+	}
+
+
+
+	//end of namespace
+}
+
+
